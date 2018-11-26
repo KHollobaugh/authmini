c1b6e620a7b8b0d1abf08bccf3d00f627a21f1b1
KHollobaugh <khollobaugh@gmail.com>
2018-11-26 14:17:08 -0500

    class follow-along

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const bcrypt = require('bcryptjs');
 
 const db = require('./database/dbConfig.js');
 
@@ -8,6 +9,53 @@ const server = express();
 server.use(express.json());
 server.use(cors());
 
+server.post('/api/register', (req, res) => {
+  //grab username from body
+  const creds = req.body;
+  //generate hash from password
+  const hash = bcrypt.hashSync(creds.password, 4)
+  // override user.password with the hash
+  creds.password = hash;
+  //save user to database
+  db('users')
+  .insert(creds)
+  .then(ids => {
+    res.status(201).json(ids);
+  })
+  .catch(err => json(err));
+})
+
+
+
+server.post('/api/login', (req, res) => {
+  //grab username from body
+  const creds = req.body;
+  db('users')
+  .where({ username: creds.username })
+  //generate hash from password
+  .first()
+  .then( user => {
+    if (user && bcrypt.compareSync(creds.password, user.password)
+) {
+  //passwords match and user exists
+  res.status(200).json({ message: 'welcome!' })
+    } else {
+      //username invalid or password wrong
+      res.status(401).json({ message: 'you shall not pass!'})
+    }
+  })
+  .catch(err => res.json(err)) //rounds is 2^X
+  //override user.password with the hash
+//   creds.password = hash;
+//   //save user to database
+//   db('users')
+//   .insert(creds)
+//   .then(ids => {
+//     res.status(201).json(ids);
+//   })
+//   .catch(err => json(err));
+})
+
 server.get('/', (req, res) => {
   res.send('Its Alive!');
 });
@@ -15,7 +63,7 @@ server.get('/', (req, res) => {
 // protect this route, only authenticated users should see it
 server.get('/api/users', (req, res) => {
   db('users')
-    .select('id', 'username')
+    .select('id', 'username', 'password')
     .then(users => {
       res.json(users);
     })
